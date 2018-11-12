168c3723b41945b5d403674d576933158dde247f
Dave Calnan <d@ve.ie>
2018-11-12 18:29:43 +0000

    Update components/post-preview.vue
    Replace <a> with <nuxt-link>

--- components/post-preview.vue
+++ components/post-preview.vue
@@ -2,15 +2,15 @@
   <div>
     <figure class="bg-white rounded shadow-lg">
       <header>
-        <a :href="path">
+        <nuxt-link :href="path">
           <graphcms-image :image="post.image" :width="356" :height="200" class="w-full rounded-t border-b-4 border-red"/>
-        </a>
+        </nuxt-link>
       </header>
       <main class="p-4">
         <p class="uppercase tracking-wide text-xs font-bold text-grey-darkest">
-          <a :href="category" class="text-grey-darkest no-underline">{{ post.category }}</a> | {{ post.readingTime }} min read
+          <nuxt-link :href="category" class="text-grey-darkest no-underline">{{ post.category }}</nuxt-link> | {{ post.readingTime }} min read
         </p>
-        <a :href="path" class="block text-2xl text-black no-underline my-2 font-serif">{{ post.title }}</a>
+        <nuxt-link :href="path" class="block text-2xl text-black no-underline my-2 font-serif">{{ post.title }}</nuxt-link>
         <p class="mt-4 mb-2 font-serif text-grey-darkest">
           {{ post.body | truncate(115, '...') }}
         </p>
