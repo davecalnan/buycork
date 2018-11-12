4cc7a64361fbde70f01e4e997a9fd2f212c1e412
Dave Calnan <d@ve.ie>
2018-11-12 14:51:24 +0000

    Update pages/local-alternatives-to/_slug.vue
    Remove blog and about sections

--- pages/local-alternatives-to/_slug.vue
+++ pages/local-alternatives-to/_slug.vue
@@ -27,44 +27,6 @@
         </div> -->
       </section>
     </ais-index>
-    <section class="bg-grey-light">
-      <div class="md:pt-24 md:pr-8 pb-0 md:pl-24 flex flex-column">
-        <svg class="h-96 flex-no-shrink text-grey-darker fill-current hidden md:inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M16 2h4v15a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V0h16v2zm0 2v13a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4h-2zM2 2v15a1 1 0 0 0 1 1h11.17a2.98 2.98 0 0 1-.17-1V2H2zm2 8h8v2H4v-2zm0 4h8v2H4v-2zM4 4h8v4H4V4z"/></svg>
-        <div class="pt-8 px-8 md:p-0 md:pt-12 md:pl-12 md:w-144">
-          <div class="flex items-center mb-4">
-            <svg class="md:hidden h-24 mr-4 flex-no-shrink text-grey-darker fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M16 2h4v15a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V0h16v2zm0 2v13a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4h-2zM2 2v15a1 1 0 0 0 1 1h11.17a2.98 2.98 0 0 1-.17-1V2H2zm2 8h8v2H4v-2zm0 4h8v2H4v-2zM4 4h8v4H4V4z"/></svg>
-            <nuxt-link to="/blog" class="font-serif text-black text-4xl no-underline">Our <br class="inline md:hidden">Blog &rarr;</nuxt-link>
-          </div>
-          <p class="text-black text-xl my-4">
-            Independent businesses need your support now more than ever. We'll be sharing how and why to support them along with sharing their stories.
-          </p>
-          <button-link href="/blog" type="standard" nuxt>Visit the blog</button-link>
-        </div>
-      </div>
-      <scroll-container>
-        <post-preview v-for="post in posts" :key="post.id" :post="post" class="w-80 md:w-96 px-4 md:px-6 flex-no-shrink"/>
-      </scroll-container>
-    </section>
-    <!-- <section class="bg-grey-darker text-white">
-      <div class="p-8 md:pt-24 md:pr-12 md:pb-12 md:pl-24 flex flex-column">
-        <svg class="hidden md:block h-96 flex-no-shrink fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M3 6c0-1.1.9-2 2-2h8l4-4h2v16h-2l-4-4H5a2 2 0 0 1-2-2H1V6h2zm8 9v5H8l-1.67-5H5v-2h8v2h-2z"/></svg>
-        <div class="md:pt-12 md:pl-12 md:w-144">
-          <div class="flex items-center mb-4">
-            <svg class="md:hidden h-24 mr-4 flex-no-shrink fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M3 6c0-1.1.9-2 2-2h8l4-4h2v16h-2l-4-4H5a2 2 0 0 1-2-2H1V6h2zm8 9v5H8l-1.67-5H5v-2h8v2h-2z"/></svg>
-            <h1 class="font-serif text-4xl">Our <br class="inline md:hidden">Mission<span class="hidden xs:inline"> &rarr;</span></h1>
-          </div>
-          <div class="text-grey-lightest text-xl my-4">
-            <p class="mb-4">
-              Irish businesses are being outperformed online.
-            </p>
-            <p>
-              We are determined to empower Irish businesses with the knowledge, tools, and help to succeed.
-            </p>
-          </div>
-          <button-link href="/about" type="standard" nuxt>Why we do it</button-link>
-        </div>
-      </div>
-    </section> -->
   </main>
 </template>
 
