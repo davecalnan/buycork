eb8e49c1ec5494c82433ccffdb1519ab3e9eeda2
Dave Calnan <d@ve.ie>
2018-11-20 18:10:55 +0000

    Add redirect to typeform at /submit

---
+++ middleware/redirect-to-submit.js
@@ -0,0 +1,4 @@
+export default function ({ store, redirect }) {
+  const formURL = 'https://davecalnan.typeform.com/to/NaZY4J'
+  return redirect(formURL)
+}
--- pages/submit.vue
+++ pages/submit.vue
@@ -1,3 +1,11 @@
 <template>
-  <p>Submit form will go here.</p>
+  <p>
+    You will be redirected shortly. <a href="">Click here</a> if you are not redirected in a few seconds.
+  </p>
 </template>
+
+<script>
+export default {
+  middleware: 'redirect-to-submit'
+}
+</script>
